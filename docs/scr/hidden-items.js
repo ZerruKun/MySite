e207cf4d18e0f4bedf7b273de573eafba7f7ab41
Tabalin Nick <Zerru_Kun@yahoo.com>
2020-09-16 18:13:42 +0300

    Эксперимент с switch.

--- docs/scr/hidden-items.js
+++ docs/scr/hidden-items.js
@@ -3,14 +3,21 @@ let larrow = document.querySelector('.larrow');
 let rarrow = document.querySelector('.rarrow');
 let item = projects[0];
 
+
 rarrow.onclick = function() {
   switch (item) {
     case projects[0]:
-
+      projects[0].classList.add('visual-hidden');
+      projects[1].classList.remove('visual-hidden');
+      item = projects[1];
+      console.log(item);
       break;
-      
-    case projects[1]:
 
+    case projects[1]:
+      projects[1].classList.add('visual-hidden');
+      projects[2].classList.remove('visual-hidden');
+      item = projects[2];
+      console.log(item);
       break;
 
     case projects[2]:
@@ -24,9 +31,8 @@ rarrow.onclick = function() {
     default:
 
   }
-  console.log(item);
-}
 
+}
 
 /*
 let detailedEducation = document.querySelector(".detailed1");
